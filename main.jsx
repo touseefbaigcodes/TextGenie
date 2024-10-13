99f9e5fe569830831906c1ecb136a6f32f419bcd
Touseef Baig <154717497+touseefbaigcodes@users.noreply.github.com>
2024-10-13 19:13:01 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,42 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import UseCallBack from './Hooks/UseCallbackFolder/UseCallBack';
+import Practice from './Practice';
+// import Forms from './ReactHookForm/Forms';
+// import Aboueducer from './UseReducer';
+// import { Provider } from react-redux';
+// import { Stot from './About';
+// import Myarticles from './Myarticles';
+// import UseRre } from './Redux Tookit/Redux toolkid(Synchronous)/Store';
+// import { Counter } from './Redux Tookit/Redux toolkid(Synchronous)/App';
+// import store from './Redux Tookit/Redux toolkit(Asynchronous)/Store';
+// import UserComponent from './Redux Tookit/Redux tool`kit(Asynchronous)/App';
+import NavBar from './BootStrap Landing Page/NavBar';
+
+import App from './P1 TextGenie/App';
+// import App from './P2 CurrentChronicles/App';
+
+
+createRoot(document.getElementById('root')).render( 
+       // <StrictMode>
+<div>
+         {/* <Forms/>  */}
+  {/* <Practice/> */}
+  {/* <NavBar/> */}
+  {/* <UseCallBack/> */}
+  {/* <Provider store={store}> */}
+        {/* < About/> */}
+        {/* <Myarticles/> */}
+        {/* <UseReducer/> */}
+        {/* <App/> */}
+{/* <UserComponent/> */}
+       {/* <Counter/> */}
+  {/* </Provider> */}
+  
+
+  <App/>
+</div>
+//   </StrictMode>
+
+);  
+  
